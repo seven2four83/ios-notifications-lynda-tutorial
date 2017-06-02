232f74cf8228bdbefb2eaa607c0d4d24bdcd5179
7to4 <jagan.moorthy@sap.com>
2017-06-02 08:42:10 +0530

    Requesting notification authorization

--- HuliPizzaNotification/ViewController.swift
+++ HuliPizzaNotification/ViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import UserNotifications
+
 
 class ViewController: UIViewController {
+    
+    private var isNotificationAccessGranted  = false
+    
 
     @IBAction func schedulePizza(_ sender: UIButton) {
+        guard isNotificationAccessGranted else {
+            return
+        }
     }
     @IBAction func makePizza(_ sender: UIButton) {
+        guard isNotificationAccessGranted else {
+            return
+        }
     }
     
     @IBAction func nextPizzaStep(_ sender: UIButton) {
+        guard isNotificationAccessGranted else {
+            return
+        }
     }
     
     @IBAction func viewPendingPizzas(_ sender: UIButton) {
+        guard isNotificationAccessGranted else {
+            return
+        }
     }
     
     @IBAction func viewDeliveredPizzas(_ sender: UIButton) {
+        guard isNotificationAccessGranted else {
+            return
+        }
     }
     
     @IBAction func removeNotification(_ sender: UIButton) {
+        guard isNotificationAccessGranted else {
+            return
+        }
     }
     
     
@@ -33,11 +56,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (isGranted, error) in
+            self.isNotificationAccessGranted = isGranted
+            if !self.isNotificationAccessGranted{
+                let alert = UIAlertController(title: "Need Access", message: "Kindly provide notification access", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { (bool, error) in
+                    })
+                })
+                alert.addAction(alertAction)
+                self.show(alert, sender: self)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
