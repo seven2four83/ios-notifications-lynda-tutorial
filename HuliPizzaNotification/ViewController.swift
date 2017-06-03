c85063c7dbabebd114f43b820b657f441bbcd4cd
7to4 <vikibornin1991@gmail.com>
2017-06-03 22:36:45 +0530

    Adding notification and enabling in-app notification

--- HuliPizzaNotification/ViewController.swift
+++ HuliPizzaNotification/ViewController.swift
@@ -12,18 +12,48 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
+    private let SCHEDULED_PIZZA_IDENTIFIER = "scheduled.pizza.notification"
+    private let MAKE_PIZZA_IDENTIFIER = "make.pizza.identifier"
+    
     private var isNotificationAccessGranted  = false
     
+    private func makePizzaContent() -> UNMutableNotificationContent{
+        let content = UNMutableNotificationContent()
+        content.title = "Make Pizza"
+        content.body = "Let's make some pizza"
+        content.userInfo = ["step":0]
+        return content
+    }
 
+    private func createNotification(identifier : String, content : UNNotificationContent, trigger: UNNotificationTrigger?){
+        let notificationRequest = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(notificationRequest) { (error) in
+            if nil != error{
+                
+            }
+        }
+    }
+    
     @IBAction func schedulePizza(_ sender: UIButton) {
         guard isNotificationAccessGranted else {
             return
         }
+        let content = UNMutableNotificationContent()
+        content.title = "Scehduled Pizza"
+        content.body = "Time to  make some pizza"
+        let dateComponents : Set<Calendar.Component> = [.minute,.hour,.second]
+        var dateToMatch = Calendar.current.dateComponents(dateComponents, from: Date())
+        dateToMatch.second = dateToMatch.second! + 5
+        let trigger  = UNCalendarNotificationTrigger(dateMatching: dateToMatch, repeats: false)
+        createNotification(identifier: SCHEDULED_PIZZA_IDENTIFIER, content: content, trigger: trigger)
     }
     @IBAction func makePizza(_ sender: UIButton) {
         guard isNotificationAccessGranted else {
             return
         }
+        let content = makePizzaContent()
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        createNotification(identifier: MAKE_PIZZA_IDENTIFIER, content: content, trigger: trigger)
     }
     
     @IBAction func nextPizzaStep(_ sender: UIButton) {
@@ -51,11 +81,10 @@ class ViewController: UIViewController {
     }
     
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().delegate = self
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (isGranted, error) in
             self.isNotificationAccessGranted = isGranted
             if !self.isNotificationAccessGranted{
@@ -77,5 +106,12 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UNUserNotificationCenterDelegate{
+    //MARK: - UNUserNotificationCenterDelegate methods
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.badge,.sound])
+    }
 }
 
